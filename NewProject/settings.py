ca53488b5c3e915b1c4b5bfac4eac1f916c1294f
mubarakebb <47586709+mubarakebb@users.noreply.github.com>
2022-12-13 14:20:26 +0800

    Update settings.py

--- NewProject/settings.py
+++ NewProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c%a36rfw2b1xt*yto5b4h$-h2i-2&5yr#xf@wui0$-84k_*7c^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'https://email-spam-detection-system-production.up.railway.app/']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,10 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = [
-    os.path.join(BASE_DIR, "staticfiles"),
-    'staticfiles/', 
-]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     'static/',
